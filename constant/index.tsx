fe68e7f5516578e34675dbc69e4cac47c30193b7
dkhoi1302 <5001104077@student.hcmue.edu.vn>
2024-09-24 21:59:02 +0700

    Initial commit

---
+++ constant/index.tsx
@@ -0,0 +1,115 @@
+export const userInfo = [
+
+
+    {
+        background: "/khoi2.jpg",
+        avatar: '/khoi3.jpg',
+        name: 'Nguyễn Đăng Khôi',
+        info: 'Grow And Perfect Yourself',
+
+    }
+
+
+
+];
+
+export const setting = [
+
+
+    {
+        school: "Ho Chi Minh City University of Education (HCMUE)",
+        xschool: '/https://www.facebook.com/@HCMUE.VN',
+        distric: 'Thủ Dầu Một - Bình Dương - Vietnam',
+        xdistric: 'https://www.google.com/maps/place/cam+ranh/data=!4m2!3m1!1s0x3170ed090a3e294d:0x2afb5944a8a18f6a?sa=X&ved=1t:155783&ictx=111',
+        email: 'dangkhoi13022006@gmail.com',
+        website: 'dkhoi1302.vercel.app',
+
+    }
+
+
+
+];
+
+
+export const secctionCopy = [
+
+
+    {
+        image: "https://th.bing.com/th/id/R.a569763f29c9cc20b167d4c47e9b95fd?rik=0VUzdeUXmeT7OA&riu=http%3a%2f%2fis1.mzstatic.com%2fimage%2fthumb%2fPurple127%2fv4%2fba%2fa3%2fec%2fbaa3ec06-4110-43db-0fe3-5d728002a2b0%2fsource%2f1200x630bb.jpg&ehk=VO43hY4xqXmzYPiczcKkYpPvwDKj4%2ftG4f8iLd2UBII%3d&risl=&pid=ImgRaw&r=0",
+        title: 'Instagram',
+        name: 'Nguyễn Đăng Khôi',
+        link: 'https://www.instagram.com/dangkhoi9925?igsh=aDEzZmR1cmp1czN5&utm_source=qr',
+
+    },
+    {
+        image: "https://github.com/fluidicon.png",
+        title: 'Github',
+        name: 'dkhoi1302',
+        link: 'https://github.com/dkhoi1302',
+
+    },
+    {
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYWgI-Qu8PxBhwz0Tc9McWWIxlxrCJZGNQdw&s",
+        title: 'Liên Quân',
+        name: 'ɪᴄᴇ•ᴄᴀᴛᴀɴᴅғɪsʜ',
+        link: '#',
+
+    },
+    {
+        image: "https://th.bing.com/th/id/OIP.Xo_FCK4zl3xBtFXByduYzQHaHa?rs=1&pid=ImgDetMain",
+        title: 'Facebook',
+        name: 'Nguyễn Đăng Khôi',
+        link: 'https://www.facebook.com/profile.php?id=100083812073511',
+
+    },
+    {
+        image: "https://stc-zaloprofile.zdn.vn/pc/v1/images/zalo_sharelogo.png",
+        title: 'Zalo',
+        name: 'Nguyễn Đăng Khôi',
+        link: 'https://zalo.me/0849130206',
+
+    },
+
+
+];
+
+export const modalssss = [
+
+
+    {
+        image: "https://dongphuchaianh.vn/wp-content/uploads/2023/06/y-nghia-logo-bidv.jpg",
+        title: 'BIDV',
+        name: 'Get QR Code',
+        modal: '/khoi1.png',
+
+
+    },
+    // {
+    //     image: "https://laserbinhthuan.com/wp-content/uploads/2021/10/meta-logo-facebook.jpg",
+    //     title: 'Certificate',
+    //     name: 'Meta',
+    //     modal: 'https://raw.githubusercontent.com/levinhkhangzz/assets/main/z5164475138499_f66102b26d26a8b2e05af624fffab1e0.jpg',
+
+
+    // },
+    // {
+    //     image: "https://th.bing.com/th/id/OIP.kY7h_2SMSJv6fdNrPrAROQAAAA?w=400&h=400&rs=1&pid=ImgDetMain",
+    //     title: 'Certificate',
+    //     name: 'Google Cloud',
+    //     modal: 'https://raw.githubusercontent.com/levinhkhangzz/assets/main/z5164475157110_f11989f1964f1fb581f7d2efd0bb5788.jpg',
+
+
+    // },
+    // {
+    //     image: "https://th.bing.com/th/id/OIP.ysdCJiI4OEy9VF-5ituvWwAAAA?w=300&h=300&rs=1&pid=ImgDetMain",
+    //     title: 'Certificate',
+    //     name: 'Amazon Web Service',
+    //     modal: 'https://raw.githubusercontent.com/levinhkhangzz/assets/main/z5164475154720_a0157710d39eed70389cf7b43ff6793a.jpg',
+
+
+    // },
+
+
+
+
+];
